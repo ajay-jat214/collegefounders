7b450bbe0502ae473d7701bc065a1308774219b6
Ajay Jat <jatajay004@gmail.com>
2021-04-22 13:27:14 +0530

    dslfkj

--- backend/collegefounders/src/components/Article.js
+++ backend/collegefounders/src/components/Article.js
@@ -1,12 +1,23 @@
-import React, { forwardRef, useRef } from 'react';
-import {VerticalAlignTopOutlined} from '@ant-design/icons';
+import React, { forwardRef, useRef } from "react";
+import { VerticalAlignTopOutlined } from "@ant-design/icons";
 
 const Article = forwardRef(({ onBackClick }, ref) => {
-    return (
-        <article>
-            <VerticalAlignTopOutlined onClick={onBackClick} style={{fontSize:'46px',color: 'white',background:"#f44336",borderRadius:"10px",position:"fixed",bottom:"24px",right:"24px"}}/>
-        </article>
-    );
-})
+  return (
+    <article>
+      <VerticalAlignTopOutlined
+        onClick={onBackClick}
+        style={{
+          fontSize: "46px",
+          color: "white",
+          background: "#f44336",
+          borderRadius: "10px",
+          position: "fixed",
+          bottom: "24px",
+          right: "24px",
+        }}
+      />
+    </article>
+  );
+});
 
 export default Article;
