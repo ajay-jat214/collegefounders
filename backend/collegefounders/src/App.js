a3db8e5cd8ea019267b288d7b8e36542cc182fc4
Ajay Jat <jatajay004@gmail.com>
2021-04-22 10:17:54 +0530

    dslfksldk

--- backend/collegefounders/src/App.js
+++ backend/collegefounders/src/App.js
@@ -36,7 +36,7 @@ function App() {
 
   useEffect(
     () => {
-      fetch("https://collegefounders.herokuapp.com/data", {
+      fetch("https://vibrant-mayer-9ca16c.netlify.app/data", {
         method: "post",
         headers: { Authentication: "Content-Type:application/json" },
         body: JSON.stringify({ collegeId: collegeId }),
